e419501e5a949f664f520cefe843002924b5fdb6
vbersenev72 <vbersenev72@gmail.com>
2026-01-17 02:19:42 +0500

    fix schema

--- types/auth.ts
+++ types/auth.ts
@@ -2,15 +2,15 @@ import z from "zod";
 import { validate } from "../backend/src/middlewares/validate";
 
 export const registerSchema = z.object({
-    username: z.string().refine((val) => val.length < 10, 'Usename length is more than 10 characters'),
-    password: z.string().refine((val) => val.length > 8, 'Password length is less than 9 characters')
+    username: z.string(),
+    password: z.string()
 })
 export type RegisterRequestData = z.infer<typeof registerSchema>
 export const validateRegisterSchema = validate(registerSchema)
 
 export const loginSchema = z.object({
-    username: z.string().refine((val) => val.length < 10, 'Usename length is more than 10 characters'),
-    password: z.string().refine((val) => val.length > 8, 'Password length is less than 9 characters')
+    username: z.string(),
+    password: z.string()
 })
 export type LoginRequestData = z.infer<typeof registerSchema>
 export const validateLoginSchema = validate(loginSchema)
